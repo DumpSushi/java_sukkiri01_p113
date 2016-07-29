3dcc8621e8e51b7f5582768063af01fd41700226
DumpSushi <DumpSushi@users.noreply.github.com>
2016-07-29 14:11:48 +0900

    インスタンスの等価判定。

---
+++ src/p113/Account.java
@@ -0,0 +1,20 @@
+package p113;
+
+public class Account {
+	String accountNo;
+
+	public boolean equals(Object o)
+	{
+		if(o == this)
+			return true;
+		if(o==null)
+			return false;
+		if(!(o instanceof Account))
+			return false;
+		Account r = (Account)o;
+		if(this.accountNo.trim().equals(r.accountNo.trim()))
+			return true;
+		return false;
+	}
+
+}
--- src/p113/Main.java
+++ src/p113/Main.java
@@ -1,12 +1,28 @@
 package p113;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		// 4-2 インスタンスの文字列表現。
-		Hero h1 = new Hero("aaa");
-		System.out.println(h1);
+		{
+			Hero h1 = new Hero("aaa");
+			System.out.println(h1);
+		}
+
+		// 4-3 インスタンスの等価判定。
+		{
+			List<Hero> list = new ArrayList<Hero>();
+			Hero h1 = new Hero("ミナト");
+			list.add(h1);
+			System.out.println("要素数=" + list.size());
+			h1 = new Hero("ミナト");
+			list.remove(h1);
+			System.out.println("要素数=" + list.size());	// 要素が減っていない。
+		}
 	}
 
 }
